3d7289a0a6d22d4f55ef94fb4e5ad156650df23f
Justin Page <xjustinpagex@gmail.com>
2014-03-23 22:37:33 -0700

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+Shelby Web Design
+=================
+
+Project Info
+------------
+About: Coded a design for a student back in my baby days of a naive coder.
+
+*May the JS gods forgive me for such spaghetti code*
---
+++ _JS/nav.js
@@ -0,0 +1,96 @@
+/* ============================================ */
+/*     Coded by Page                            */
+/*     Navigation Animation && Hover States 
+/*     =Additional lemmonSlider.js && gmap3.js 
+/* ============================================ */
+
+(function() {
+	
+	var wrap = $('div.wrap');
+	var main = $('div.grid-container');
+	var mHeight = main.height();
+	var fButton;
+	
+
+	$('div.buttons a').on('mouseenter', function() {
+		$(this).prev().css('color','#e3ff00');
+		}).on('mouseleave', function() {
+			$(this).prev().css('color', 'white');
+		});
+
+	$('div.buttons').on('click','a', function(e) {
+		var sButton = $(this).prev().prev();
+		var href = $(this).attr('href');
+
+
+		if(main.height() == mHeight) {
+			wrap.animate({height: '220px', opacity: 1.0},300,false);
+			wrap.load(href + ' .container').promise().done(function() {
+				sButton.animate({opacity:1.0},250,false);
+				$('div.slider').lemmonSlider({
+    				'infinite' : true
+				});
+
+				// launch map if Contact is clicked
+				$("#my_map").gmap3({
+					map:{
+						options:{
+							center: [34.675464,-118.184803],
+							zoom: 14
+						}
+					},
+					marker:{
+						latLng: [34.675464,-118.184803],
+					},
+				});
+
+				// launch calendar if Home is clicked
+				$('#date3').DatePicker({
+					flat: true,
+					date: ['2013-05-20','2013-05-27'],
+					current: '2013-05-20',
+					calendars: 2,
+					mode : 'range',
+					starts: 1
+				});
+			});
+
+		} else {
+			fButton.animate({opacity:0.0},300,false).delay(600);
+			wrap.animate({height: '0px', opacity: 0.0},300,false)
+				.promise().done(function() {
+					wrap.animate({height: '220px', opacity: 1.0},300,false);
+					wrap.load(href + ' .container').promise().done(function() {
+						sButton.animate({opacity:1.0},250,false);
+						$('div.slider').lemmonSlider({
+    						'infinite' : true
+						});
+
+						// launch map if Contact is clicked
+						$("#my_map").gmap3({
+							map:{
+								options:{
+									center: [34.675464,-118.184803],
+									zoom: 14
+								}
+							},
+							marker:{
+								latLng: [34.675464,-118.184803],
+							},
+						});
+						$('#date3').DatePicker({
+							flat: true,
+							date: ['2013-04-20','2013-04-27'],
+							current: '2013-05-20',
+							calendars: 2,
+							mode : 'range',
+							starts: 1
+						});
+					});
+				});
+		}
+
+		fButton = sButton;
+		e.preventDefault();
+	});
+})();
